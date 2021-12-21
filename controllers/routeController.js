97f390718da4155becab5e2c6f1e8e17ad008c70
mombengue <doumomb2010@gmail.com>
2021-12-21 19:34:45 +0100

    change header banner

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
+/* const passport = require('passport'); */
 const article = require('../models/articleModels');
 const categorie = require('../models/categorieModels');
 /* const user = require('../models/userModels'); */
