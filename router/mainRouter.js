5444c4540d4103a36913434a30fe58925fdb868d
mombengue <doumomb2010@gmail.com>
2021-12-23 11:07:34 +0100

    Add express session flash bcrypt

--- router/mainRouter.js
+++ router/mainRouter.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
-/* const passport = require('passport'); */
 const article = require('../models/articleModels');
 const categorie = require('../models/categorieModels');
-/* const user = require('../models/userModels'); */
 
 router.get('/', (req, res) => {
     var articles = article.getArticles;
@@ -24,29 +22,6 @@ router.get('/connexion', (req, res) => {
     res.render('pages/login');
 });
 
-
-/* router.get('/register', (req,res) => {
-	res.render('pages/register');
-});
-
-router.post('/register', (req,res) => {
-	var newUser = user.createUser(
-        {
-            name:req.body.name,
-            email:req.body.email,
-            password:req.body.password
-        });
-});
-
-
-router.post('/login', passport.authenticate('local', 
-    {successRedirect: '/panier', failureRedirect: '/login'})
-);
-
-router.get('/logout', (req,res) => {
-	req.logout();
-	res.redirect('/');
-}); */
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,11 +8,12 @@ const dbConfig = require("./config/dbConfig");
 const mainRouter = require('./router/mainRouter');
 const adminRouter = require('./router/adminRouter');
 
-/* const User = require('../database/userDatabase'); */
-
-/* const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const passportLocalMongoose = require('passport-local-mongoose'); */
+const passport = require('passport');
+const initializePassport = require('./passport-config')
+initializePassport(
+    passport, 
+    email => users.find(user => user.email === email) 
+    )
 
 dbConfig.dbconnexion();
 
@@ -21,13 +22,6 @@ app.set('views', __dirname + '/views');
 app.set('view engine','ejs');
 
 app.use(express.static(__dirname + '/public'));
-
-/* app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use( new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser()); */
 
 // Route
 app.use('/', mainRouter);
