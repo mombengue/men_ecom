354bda1ee0ac69284fd76903556543a488d4f191
Mamadou MBENGUE <31158553+mombengue@users.noreply.github.com>
2022-01-02 12:57:54 +0100

    Add config js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const dbconnexion = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/hijab', () => {
+        await mongoose.connect(process.env.MONGO_URL, () => {
             console.log('Successfully connected to mongodb');
         });
     } 
